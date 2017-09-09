b9b812a17f8d2a616ca677ff339cf42135cbc74b
David Cohen <david@fictivekin.com>
2017-09-09 03:11:16 -0400

    code cleanup

--- lesson_map.js
+++ lesson_map.js
@@ -1,13 +1,22 @@
-// parse from map
+/**
+ * scrapes the lesson list from  https://www.freecodecamp.org/map into
+ * a 3d array and copies a json string of the result, e.g.
+ * 
+ * 
+ *  [
+ *      [
+ *          "section1", ["asg1", "asg2", ...],
+ *          "section2", ["asg1", "asg2", ...],
+            ...
+ *      ]
+ *  ]
+*/
 
-// https://www.freecodecamp.org/map
+const certification = document.querySelector('#collapseFront-End-Development-Certification>#nested')
 
+const sections = Array.from(certification.querySelectorAll('h3>a'))
 
- certification = document.querySelector('#collapseFront-End-Development-Certification>#nested')
-
- sections = Array.from(certification.querySelectorAll('h3>a'))
-
-  sectionData = sections.map(el => [el.innerText, Array.from(certification.querySelectorAll(`${el.href.slice(el.href.indexOf('#'))}>p>a`)).map(x => x.innerText.replace(/ (in)?complete$/gi, ''))]);
+const  sectionData = sections.map(el => [el.innerText, Array.from(certification.querySelectorAll(`${el.href.slice(el.href.indexOf('#'))}>p>a`)).map(x => x.innerText.replace(/ (in)?complete$/gi, ''))]);
 
 copy(JSON.stringify(sectionData))
 
