417d92fae9be9e24adad236455a0e5b3155ce133
David Cohen <david@fictivekin.com>
2017-09-14 12:16:54 -0400

    🚀 Increment version number to deploy a42c8c9 section name fix (PR #1). Thanks @tortxof!

--- bulk-fcc-progress.js
+++ bulk-fcc-progress.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FCC Progress
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       David Cohen
 // @updateURL    https://raw.githubusercontent.com/dacohenii/fcc-progress/master/bulk-fcc-progress.js
